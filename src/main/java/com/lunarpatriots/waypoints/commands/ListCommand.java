fd974c4fdadd20ce781b46a746aaa06958a352ba
theLANister <mjremos19@gmail.com>
2021-06-14 16:59:54 +0800

    Fixed Region list outputs

--- src/main/java/com/lunarpatriots/waypoints/commands/ListCommand.java
+++ src/main/java/com/lunarpatriots/waypoints/commands/ListCommand.java
@@ -6,6 +6,10 @@ import com.lunarpatriots.waypoints.util.DataFileUtil;
 import com.lunarpatriots.waypoints.util.LogUtil;
 import com.lunarpatriots.waypoints.util.MessageUtil;
 import com.lunarpatriots.waypoints.util.ValidatorUtil;
+
+import org.bukkit.World;
+import org.bukkit.WorldType;
+import org.bukkit.World.Environment;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -19,69 +23,89 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
- * Created By: theLANister Date created: 06/13/2021
+ * Created By: theLANister Date created: 06/13/2021 Date Modified: 6/14/2021
  */
 
 public class ListCommand implements TabExecutor {
+    enum Region {
+        OVERWORLD("overworld", ""), NETHER("nether", "_nether"), END("end", "_the_end");
+
+        String keyword;
+        String value;
+
+        Region(final String keyword, final String value) {
+            this.keyword = keyword;
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public String getKeyword() {
+            return keyword;
+        }
+
+        public static String getRegion(final String keys) {
+            String regionValue = "";
+            for (Region regions : Region.values()) {
+                if (regions.keyword.equals(keys)) {
+                    regionValue = regions.getValue();
+                }
+            }
+
+            return regionValue;
+        }
+
+        public static String getRegionMessage(final String keys) {
+            String regionMessage = "";
+            for (Region regions : Region.values()) {
+                if (regions.keyword.equals(keys)) {
+                    if (regions.keyword.equals("overworld")) {
+                        regionMessage = ChatColor.AQUA + "Overworld " + ChatColor.GREEN + "Waypoints: \n";
+                    } else if (regions.keyword.equals("nether")) {
+                        regionMessage = ChatColor.RED + "Nether " + ChatColor.GREEN + "Waypoints: \n";
+                    } else if (regions.keyword.equals("end")) {
+                        regionMessage = ChatColor.LIGHT_PURPLE + "End " + ChatColor.GREEN + "Waypoints: \n";
+                    }else {
+                        regionMessage = "Invalid Region!";
+                    }
+                } 
+            }
+
+            return regionMessage;
+        }
+    };
 
     @Override
     public boolean onCommand(final CommandSender commandSender, final Command command, final String s,
             final String[] strings) {
         final Player player = (Player) commandSender;
+
         final WaypointRepository waypointrepo = new WaypointRepository();
         String msg = "";
         String waypoints = "";
-        switch (strings[0]) {
-            case ("overworld"):
-                final List<Waypoint> waypointOver = waypointrepo.getWaypoints(player.getWorld().getName());
+        List<Waypoint> waypointRegion = new ArrayList<Waypoint>();
 
-                waypoints = waypointOver.stream().map(Waypoint::getName).collect(Collectors.joining("\n"));
-
-                if (waypoints.length() != 0) {
-                    msg = ChatColor.AQUA + "Overworld " + ChatColor.GREEN + "Waypoints: \n";
-                    msg += String.format("- %s", waypoints);
-                    MessageUtil.success(player, msg);
-                } else {
-                    msg += "There are no waypoints available for this region!";
-                    MessageUtil.fail(player, msg);
-                }
-                break;
-            case ("nether"):
-                final List<Waypoint> waypointNether = waypointrepo
-                        .getWaypoints(player.getWorld().getName() + "_nether");
-                waypoints = waypointNether.stream().map(Waypoint::getName).collect(Collectors.joining("\n"));
-
-                if (waypoints.length() != 0) {
-                    msg = ChatColor.RED + "Nether " + ChatColor.GREEN + "Waypoints: \n";
-                    msg += String.format("- %s", waypoints);
-                    MessageUtil.success(player, msg);
-                } else {
-                    msg += "There are no waypoints available for this region!";
-                    MessageUtil.fail(player, msg);
-                }
-
-                MessageUtil.success(player, msg);
-                break;
-            case ("end"):
-                final List<Waypoint> waypointEnd = waypointrepo.getWaypoints(player.getWorld().getName() + "_the_end");
-                ;
-
-                waypoints = waypointEnd.stream().map(Waypoint::getName).collect(Collectors.joining("\n"));
-                if (waypoints.length() != 0) {
-                    msg = ChatColor.LIGHT_PURPLE + "End " + ChatColor.GREEN + "Waypoints: \n";
-                    msg += String.format("- %s", waypoints);
-
-                    MessageUtil.success(player, msg);
-                } else {
-                    msg += "There are no waypoints available for this region!";
-                    MessageUtil.fail(player, msg);
-                }
-
-                break;
-            default:
-                MessageUtil.fail(player, "Invalid use of /list");
-                break;
+        if (player.getWorld().getEnvironment().equals(World.Environment.NORMAL)){
+            waypointRegion = waypointrepo.getWaypoints(player.getWorld().getName() + Region.getRegion(strings[0]));
+        } else if (player.getWorld().getEnvironment().equals(World.Environment.NETHER)){
+            waypointRegion = waypointrepo.getWaypoints(player.getWorld().getName().replace("_nether", Region.getRegion(strings[0])));
+        } else if (player.getWorld().getEnvironment().equals(World.Environment.THE_END)){
+            waypointRegion = waypointrepo.getWaypoints(player.getWorld().getName().replace("_the_end", Region.getRegion(strings[0])));
         }
+
+        waypoints = waypointRegion.stream().map(Waypoint::getName).collect(Collectors.joining("\n- "));
+        if(waypoints.length() != 0){
+            msg =  Region.getRegionMessage(strings[0]) + "- ";
+            msg += String.format("%s", waypoints);
+            MessageUtil.success(player, msg);
+        } else {
+            msg = "There are no valid waypoints on this region! ";
+            MessageUtil.fail(player, msg);
+        }
+        
+
         return true;
     }
 
