c686140383fea59d4667c68b4171ec6a90acd425
lunarpatriots <tristan.hamili@gmail.com>
2021-06-15 21:20:42 +0800

    Changed teleport animation and sound to play on original player location instead of target destination

--- src/main/java/com/lunarpatriots/waypoints/listener/SelectWaypointListener.java
+++ src/main/java/com/lunarpatriots/waypoints/listener/SelectWaypointListener.java
@@ -78,8 +78,9 @@ public class SelectWaypointListener implements Listener {
                     ExpUtil.changePlayerExp(player, -cost);
                 }
 
-                player.spawnParticle(Particle.PORTAL, targetLocation, 500);
-                player.playSound(targetLocation, Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+                final Location sourceLocation = player.getLocation();
+                player.spawnParticle(Particle.PORTAL, sourceLocation.add(0, 1, 0), 100);
+                player.playSound(sourceLocation, Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
                 player.teleport(targetLocation);
             } else {
                 MessageUtil.fail(player, "You do not have enough exp to fast travel to that location!");
