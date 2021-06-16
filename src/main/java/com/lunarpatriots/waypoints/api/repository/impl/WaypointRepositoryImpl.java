bb405cf29bb41b21c63476c92b4180de4a6d857f
lunarpatriots <tristan.hamili@gmail.com>
2021-06-16 17:15:53 +0800

    Remove waypoint from table if destroyed

---
+++ src/main/java/com/lunarpatriots/waypoints/api/repository/impl/WaypointRepositoryImpl.java
@@ -0,0 +1,117 @@
+package com.lunarpatriots.waypoints.api.repository.impl;
+
+import com.lunarpatriots.waypoints.MainApp;
+import com.lunarpatriots.waypoints.api.constants.SqlConstants;
+import com.lunarpatriots.waypoints.api.exceptions.DatabaseException;
+import com.lunarpatriots.waypoints.api.model.Waypoint;
+import com.lunarpatriots.waypoints.api.repository.WaypointRepository;
+import com.lunarpatriots.waypoints.api.util.SqlUtil;
+import com.lunarpatriots.waypoints.util.LogUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created By: tristan.hamili@novare.com.hk
+ * Date created: 06/16/2021
+ */
+public final class WaypointRepositoryImpl implements WaypointRepository {
+
+    private final MainApp plugin;
+
+    public WaypointRepositoryImpl(final MainApp plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void initTable() throws DatabaseException {
+        try (Connection connection = SqlUtil.getConnection(plugin);
+             PreparedStatement preparedStatement = SqlUtil
+                 .buildPreparedStatement(connection, SqlConstants.CREATE_TABLE_QUERY)) {
+
+            preparedStatement.executeUpdate();
+        } catch (final SQLException ex) {
+            LogUtil.error("SQL Error: " + ex.getErrorCode());
+            throw new DatabaseException(ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public List<Waypoint> getWaypoints() throws DatabaseException {
+        try (Connection connection = SqlUtil.getConnection(plugin);
+             PreparedStatement preparedStatement = SqlUtil
+                 .buildPreparedStatement(connection, SqlConstants.GET_QUERY);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            return SqlUtil.readResult(resultSet);
+        } catch (final SQLException ex) {
+            LogUtil.error("SQL Error: " + ex.getErrorCode());
+            throw new DatabaseException(ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public List<Waypoint> filterWaypoints(final String world) throws DatabaseException {
+        try (Connection connection = SqlUtil.getConnection(plugin);
+             PreparedStatement preparedStatement = SqlUtil
+                 .buildPreparedStatement(connection, SqlConstants.GET_FILTERED_QUERY, world);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            return SqlUtil.readResult(resultSet);
+        } catch (final SQLException ex) {
+            LogUtil.error("SQL Error: " + ex.getErrorCode());
+            throw new DatabaseException(ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public int saveWaypoint(final Waypoint waypoint) throws DatabaseException {
+        try (Connection connection = SqlUtil.getConnection(plugin);
+             PreparedStatement preparedStatement = SqlUtil
+                 .buildPreparedStatement(connection, SqlConstants.INSERT_QUERY, waypoint)) {
+
+            return preparedStatement.executeUpdate();
+        } catch (final SQLException ex) {
+            LogUtil.error("SQL Error: " + ex.getErrorCode());
+            throw new DatabaseException(ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public int updateWaypoint(final Waypoint waypoint) throws DatabaseException {
+        try (Connection connection = SqlUtil.getConnection(plugin);
+             PreparedStatement preparedStatement = SqlUtil
+                 .buildPreparedStatement(
+                     connection,
+                     SqlConstants.UPDATE_QUERY,
+                     waypoint.getXCoordinate(),
+                     waypoint.getYCoordinate(),
+                     waypoint.getZCoordinate(),
+                     waypoint.getUuid())) {
+
+            return preparedStatement.executeUpdate();
+        } catch (final SQLException ex) {
+            LogUtil.error("SQL Error: " + ex.getErrorCode());
+            throw new DatabaseException(ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public int deleteWaypoint(final String uuid) throws DatabaseException {
+        try (Connection connection = SqlUtil.getConnection(plugin);
+             PreparedStatement preparedStatement = SqlUtil
+                 .buildPreparedStatement(
+                     connection,
+                     SqlConstants.DELETE_QUERY,
+                     uuid)) {
+
+            return preparedStatement.executeUpdate();
+        } catch (final SQLException ex) {
+            LogUtil.error("SQL Error: " + ex.getErrorCode());
+            throw new DatabaseException(ex.getMessage(), ex);
+        }
+    }
+}
---
+++ src/main/java/com/lunarpatriots/waypoints/listener/DestroyWaypointListener.java
@@ -0,0 +1,72 @@
+package com.lunarpatriots.waypoints.listener;
+
+import com.lunarpatriots.waypoints.api.exceptions.DatabaseException;
+import com.lunarpatriots.waypoints.api.model.Waypoint;
+import com.lunarpatriots.waypoints.api.repository.WaypointRepository;
+import com.lunarpatriots.waypoints.util.LogUtil;
+import com.lunarpatriots.waypoints.util.ValidatorUtil;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+import java.util.List;
+
+/**
+ * Created By: lunarpatriots@gmail.com
+ * Date created: 06/16/2021
+ */
+public final class DestroyWaypointListener implements Listener {
+
+    private final WaypointRepository repository;
+
+    public DestroyWaypointListener(final WaypointRepository repository) {
+        this.repository = repository;
+    }
+
+    @EventHandler
+    public void onWaypointBreak(final BlockBreakEvent event) {
+        removeWaypoint(event.getBlock());
+    }
+
+    @EventHandler
+    public void onWaypointBurn(final BlockBurnEvent event) {
+        removeWaypoint(event.getBlock());
+    }
+
+    @EventHandler
+    public void onWaypointExplode(final EntityExplodeEvent event) {
+        final List<Block> blockList = event.blockList();
+        blockList.forEach(this::removeWaypoint);
+    }
+
+    private void removeWaypoint(final Block targetBlock) {
+        if (ValidatorUtil.isValidWaypointBlock(targetBlock)) {
+            final Sign sign = (Sign) targetBlock.getState();
+            final Waypoint waypoint = new Waypoint(targetBlock.getWorld().getName(), sign);
+
+            try {
+                final String uuid = existingUuid(waypoint.getName());
+
+                if (StringUtils.isNotBlank(uuid)) {
+                    repository.deleteWaypoint(uuid);
+                }
+            } catch (final Exception ex) {
+                LogUtil.error(ex.getMessage());
+            }
+        }
+    }
+
+    private String existingUuid(final String name) throws DatabaseException {
+        final List<Waypoint> waypoints = repository.getWaypoints();
+        return waypoints.stream()
+            .filter(waypoint -> name.equals(waypoint.getName()))
+            .findFirst()
+            .map(Waypoint::getUuid)
+            .orElse(null);
+    }
+}
